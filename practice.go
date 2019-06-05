b43e462afbce8868c611ea136b777fc972f09db3
Chanchal <ckmandal9@gmail.com>
2019-06-05 13:13:36 +0530

    Add files via upload

--- practice.go
+++ practice.go
@@ -1,4 +1,59 @@
 /**
+ * functins
+ */
+
+// package main
+
+// import(
+// 	"fmt"
+// )
+
+// func main() {
+// 	var a int = 100
+// 	var b int = 200
+// 	var ret int
+
+// 	ret = max(a, b)
+
+// 	fmt.Printf("Max value is %d", ret)
+
+// 	m, n := swap("Jim", "Umair")
+// 	fmt.Println(m, n)
+// }
+
+// func max(num1, num2 int) int {
+// 	var result int
+
+// 	if (num1 > num2) {
+// 		result = num1
+// 	} else {
+// 		result = num2
+// 	}
+
+// 	return result
+// }
+
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
+
+
+/**
+ * Print Hello World
+ */ 
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main(){
+// 	fmt.Println("Hello WOrld!")
+// }
+
+
+/**
  * Error Handling
  */
 
