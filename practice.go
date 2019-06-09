878b2dba6cd04d094f7aa9ed11d01f6eddd4a0b7
Chanchal <ckmandal9@gmail.com>
2019-06-09 14:41:06 +0530

    Add files via upload

--- practice.go
+++ practice.go
@@ -1,4 +1,25 @@
 /**
+ * Type casting
+ */
+
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	var sum int = 17
+	var count int = 5
+	var mean float32
+
+	mean = float32(sum) / float32(count)
+	fmt.Printf("Value of mean : %f\n", mean)
+}
+
+
+
+/**
  * Recursion
  */
 
